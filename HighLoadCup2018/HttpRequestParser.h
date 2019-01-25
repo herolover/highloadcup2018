8355f90b6820ba7959165753aaffb03469cd277a
Anton Zhvakin <anton.zhv@gmail.com>
2019-01-26 00:31:43 +0400

    Fixed request parser.

--- HighLoadCup2018/HttpRequestParser.h
+++ HighLoadCup2018/HttpRequestParser.h
@@ -103,59 +103,28 @@ inline ParsedRequest parse_http_request(DB &db, const boost::beast::http::reques
     auto target_parts = split(target, '/');
     if (target_parts[0] == "accounts"sv)
     {
-        if (target_parts[1] == "filter"sv && target_parts.size() == 2)
+        if (target_parts.size() == 2)
         {
-            bool is_valid = true;
-            bool has_limit = false;
-            FilterAccounts filter_accounts;
-            for (auto &param : split(params, '&'))
+            if (target_parts[1] == "filter"sv)
             {
-                auto key_value = split(param, '=');
-                if (key_value.size() != 2 || key_value[1] == ""sv)
+                bool is_valid = true;
+                bool has_limit = false;
+                FilterAccounts filter_accounts;
+                for (auto &param : split(params, '&'))
                 {
-                    is_valid = false;
-                    break;
-                }
-
-                if (key_value[0] == "limit"sv)
-                {
-                    auto [p, ec] = std::from_chars(key_value[1].data(), key_value[1].data() + key_value[1].size(), filter_accounts.limit);
-                    if (ec == std::errc())
-                    {
-                        has_limit = true;
-                    }
-                    else
+                    auto key_value = split(param, '=');
+                    if (key_value.size() != 2 || key_value[1] == ""sv)
                     {
                         is_valid = false;
                         break;
                     }
-                }
-                else if (key_value[0] != "query_id"sv)
-                {
-                    auto field_method = split(key_value[0], '_');
 
-                    if (field_method.size() == 2)
+                    if (key_value[0] == "limit"sv)
                     {
-                        auto field = make_field(field_method[0]);
-                        auto method = make_method(field_method[1]);
-                        if (field.index() != 0 && method.index() != 0)
+                        auto[p, ec] = std::from_chars(key_value[1].data(), key_value[1].data() + key_value[1].size(), filter_accounts.limit);
+                        if (ec == std::errc())
                         {
-                            auto value = std::visit([&db, &key_value, &is_valid](auto &&field, auto &&method)
-                            {
-                                using field_type = std::decay_t<decltype(field)>;
-                                using method_type = std::decay_t<decltype(method)>;
-
-                                is_valid = t_has_method<field_type, method_type>::value;
-
-                                return t_value<field_type, method_type>()(db, key_value[1]);
-                            }, field, method);
-
-                            if (!is_valid)
-                            {
-                                break;
-                            }
-
-                            filter_accounts.filter.push_back(Filter{std::move(field), std::move(method), std::move(value)});
+                            has_limit = true;
                         }
                         else
                         {
@@ -163,135 +132,195 @@ inline ParsedRequest parse_http_request(DB &db, const boost::beast::http::reques
                             break;
                         }
                     }
-                    else
+                    else if (key_value[0] != "query_id"sv)
+                    {
+                        auto field_method = split(key_value[0], '_');
+
+                        if (field_method.size() == 2)
+                        {
+                            auto field = make_field(field_method[0]);
+                            auto method = make_method(field_method[1]);
+                            if (field.index() != 0 && method.index() != 0)
+                            {
+                                auto value = std::visit([&db, &key_value, &is_valid](auto &&field, auto &&method)
+                                {
+                                    using field_type = std::decay_t<decltype(field)>;
+                                    using method_type = std::decay_t<decltype(method)>;
+
+                                    is_valid = t_has_method<field_type, method_type>::value;
+
+                                    return t_value<field_type, method_type>()(db, key_value[1]);
+                                }, field, method);
+
+                                if (!is_valid)
+                                {
+                                    break;
+                                }
+
+                                filter_accounts.filter.push_back(Filter{std::move(field), std::move(method), std::move(value)});
+                            }
+                            else
+                            {
+                                is_valid = false;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            is_valid = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (is_valid && has_limit)
+                {
+                    result = std::move(filter_accounts);
+                }
+                else
+                {
+                    std::get<BadRequest>(result).status = boost::beast::http::status::bad_request;
+                }
+            }
+            else if (target_parts[1] == "group"sv)
+            {
+                bool is_valid = true;
+                bool has_keys = false;
+                bool has_order = false;
+                bool has_limit = false;
+                GroupAccounts group_accounts;
+                for (auto &param : split(params, '&'))
+                {
+                    auto key_value = split(param, '=');
+                    if (key_value.size() != 2 || key_value[1] == ""sv)
                     {
                         is_valid = false;
                         break;
                     }
-                }
-            }
 
-            if (is_valid && has_limit)
-            {
-                result = std::move(filter_accounts);
-            }
-            else
-            {
-                std::get<BadRequest>(result).status = boost::beast::http::status::bad_request;
-            }
-        }
-        else if (target_parts[1] == "group"sv && target_parts.size() == 2)
-        {
-            bool is_valid = true;
-            bool has_keys = false;
-            bool has_order = false;
-            bool has_limit = false;
-            GroupAccounts group_accounts;
-            for (auto &param : split(params, '&'))
-            {
-                auto key_value = split(param, '=');
-                if (key_value.size() != 2 || key_value[1] == ""sv)
-                {
-                    is_valid = false;
-                    break;
-                }
-
-                if (key_value[0] == "keys"sv)
-                {
-                    has_keys = true;
-
-                    for (auto &key : split(key_value[1]))
+                    if (key_value[0] == "keys"sv)
                     {
-                        auto field = make_field(key);
-                        is_valid = std::visit([](auto &&field)
+                        has_keys = true;
+
+                        for (auto &key : split(key_value[1]))
                         {
-                            return boost::mp11::mp_contains<boost::mp11::mp_list<f_sex, f_status, f_interests, f_country, f_city>, std::decay_t<decltype(field)>>::value;
-                        }, field);
+                            auto field = make_field(key);
+                            is_valid = std::visit([](auto &&field)
+                            {
+                                return boost::mp11::mp_contains<boost::mp11::mp_list<f_sex, f_status, f_interests, f_country, f_city>, std::decay_t<decltype(field)>>::value;
+                            }, field);
+                            if (!is_valid)
+                            {
+                                break;
+                            }
+                            group_accounts.key.push_back(std::move(field));
+                        }
+
                         if (!is_valid)
                         {
                             break;
                         }
-                        group_accounts.key.push_back(std::move(field));
                     }
-
-                    if (!is_valid)
+                    else if (key_value[0] == "order"sv)
                     {
-                        break;
-                    }
-                }
-                else if (key_value[0] == "order"sv)
-                {
-                    if (key_value[1] == "1"sv)
-                    {
-                        has_order = true;
-                        group_accounts.is_large_first = false;
-                    }
-                    else if (key_value[1] == "-1"sv)
-                    {
-                        has_order = true;
-                        group_accounts.is_large_first = true;
-                    }
-                    else
-                    {
-                        is_valid = false;
-                        break;
-                    }
-                }
-                else if (key_value[0] == "limit"sv)
-                {
-                    auto[p, ec] = std::from_chars(key_value[1].data(), key_value[1].data() + key_value[1].size(), group_accounts.limit);
-                    if (ec == std::errc())
-                    {
-                        has_limit = true;
-                    }
-                    else
-                    {
-                        is_valid = false;
-                        break;
-                    }
-                }
-                else if (key_value[0] != "query_id"sv)
-                {
-                    auto field = make_field(key_value[0]);
-                    if (field.index() != 0)
-                    {
-                        Method method;
-                        Value value;
-                        std::visit([&db, &method, &value, &key_value](auto &&field)
+                        if (key_value[1] == "1"sv)
                         {
-                            using field_type = std::decay_t<decltype(field)>;
-                            if constexpr(std::is_same_v<f_birth, field_type> || std::is_same_v<f_joined, field_type>)
-                            {
-                                method = m_year();
-                                value = t_value<field_type, m_year>()(db, key_value[1]);
-                            }
-                            else
-                            {
-                                method = m_eq();
-                                value = t_value<field_type, m_eq>()(db, key_value[1]);
-                            }
-                        }, field);
-
-                        group_accounts.filter.push_back(Filter{std::move(field), std::move(method), std::move(value)});
+                            has_order = true;
+                            group_accounts.is_large_first = false;
+                        }
+                        else if (key_value[1] == "-1"sv)
+                        {
+                            has_order = true;
+                            group_accounts.is_large_first = true;
+                        }
+                        else
+                        {
+                            is_valid = false;
+                            break;
+                        }
                     }
-                    else
+                    else if (key_value[0] == "limit"sv)
                     {
-                        is_valid = false;
-                        break;
+                        auto[p, ec] = std::from_chars(key_value[1].data(), key_value[1].data() + key_value[1].size(), group_accounts.limit);
+                        if (ec == std::errc())
+                        {
+                            has_limit = true;
+                        }
+                        else
+                        {
+                            is_valid = false;
+                            break;
+                        }
                     }
+                    else if (key_value[0] != "query_id"sv)
+                    {
+                        auto field = make_field(key_value[0]);
+                        if (field.index() != 0)
+                        {
+                            Method method;
+                            Value value;
+                            std::visit([&db, &method, &value, &key_value](auto &&field)
+                            {
+                                using field_type = std::decay_t<decltype(field)>;
+                                if constexpr(std::is_same_v<f_birth, field_type> || std::is_same_v<f_joined, field_type>)
+                                {
+                                    method = m_year();
+                                    value = t_value<field_type, m_year>()(db, key_value[1]);
+                                }
+                                else
+                                {
+                                    method = m_eq();
+                                    value = t_value<field_type, m_eq>()(db, key_value[1]);
+                                }
+                            }, field);
+
+                            group_accounts.filter.push_back(Filter{std::move(field), std::move(method), std::move(value)});
+                        }
+                        else
+                        {
+                            is_valid = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (is_valid && has_keys && has_order && has_limit)
+                {
+                    result = std::move(group_accounts);
+                }
+                else
+                {
+                    std::get<BadRequest>(result).status = boost::beast::http::status::bad_request;
                 }
             }
-
-            if (is_valid && has_keys && has_order && has_limit)
+            else if (target_parts[1] == "new"sv)
             {
-                result = std::move(group_accounts);
+                AddAccount add_account{request.body().data(), request.body().size()};
+                result = std::move(add_account);
+            }
+            else if (target_parts[1] == "likes"sv)
+            {
+                AddLikes add_likes{request.body().data(), request.body().size()};
+                result = std::move(add_likes);
             }
             else
             {
-                std::get<BadRequest>(result).status = boost::beast::http::status::bad_request;
+                UpdateAccount update_account;
+                auto[p, ec] = std::from_chars(target_parts[1].data(), target_parts[1].data() + target_parts[1].size(), update_account.account_id);
+
+                if (ec == std::errc() && db.has_account(update_account.account_id))
+                {
+                    update_account.body = request.body().data();
+                    update_account.size = request.body().size();
+                    result = std::move(update_account);
+                }
+                else
+                {
+                    std::get<BadRequest>(result).status = boost::beast::http::status::not_found;
+                }
             }
         }
-        else if ((target_parts[2] == "recommend"sv || target_parts[2] == "suggest"sv) && target_parts.size() == 3)
+        else if (target_parts.size() == 3 && (target_parts[2] == "recommend"sv || target_parts[2] == "suggest"sv))
         {
             SearchForAccount search;
             auto[p, ec] = std::from_chars(target_parts[1].data(), target_parts[1].data() + target_parts[1].size(), search.account_id);
@@ -355,32 +384,6 @@ inline ParsedRequest parse_http_request(DB &db, const boost::beast::http::reques
                 {
                     std::get<BadRequest>(result).status = boost::beast::http::status::bad_request;
                 }
-            }
-        }
-        else if (target_parts[1] == "new"sv)
-        {
-            AddAccount add_account{request.body().data(), request.body().size()};
-            result = std::move(add_account);
-        }
-        else if (target_parts[1] == "likes"sv)
-        {
-            AddLikes add_likes{request.body().data(), request.body().size()};
-            result = std::move(add_likes);
-        }
-        else if (target_parts.size() == 2)
-        {
-            UpdateAccount update_account;
-            auto[p, ec] = std::from_chars(target_parts[1].data(), target_parts[1].data() + target_parts[1].size(), update_account.account_id);
-
-            if (ec == std::errc() && db.has_account(update_account.account_id))
-            {
-                update_account.body = request.body().data();
-                update_account.size = request.body().size();
-                result = std::move(update_account);
-            }
-            else
-            {
-                std::get<BadRequest>(result).status = boost::beast::http::status::not_found;
             }
         }
     }
